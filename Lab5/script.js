b964c5dabe926fbf79dc40b9a6de8f836cf192de
Taras Bondarenko <tarasbondarenko2005@gmail.com>
2024-04-27 16:18:20 +0300

    Finished Lab5

--- Lab5/script.js
+++ Lab5/script.js
@@ -1,11 +1,17 @@
 const clearBtn = document.getElementById("clear-btn");
-const matrixInputs = document.querySelectorAll(".r_c-input");
+const matrixInputs = document.querySelectorAll(".r_c-input-entry");
+const finalMatrixInputs = document.querySelectorAll(".r_c-input");
 const matrixKofs = document.querySelectorAll(".kof_sys");
 const matrixVilCh = document.querySelectorAll(".vil_ch");
 const calcBtn = document.getElementById("calculate-btn");
 const resultCont = document.getElementById("result");
 const errorMesg = document.getElementById("error");
 const closeBtn = document.getElementById("close-btn");
+const entryMatrixBtn = document.getElementById("entry-matrix-btn");
+const finalMatrixBtn = document.getElementById("final-matrix-btn");
+
+finalMatrixBtn.disabled = true;
+entryMatrixBtn.disabled = true;
 
 closeBtn.addEventListener("click", () => {
     closeError();
@@ -20,7 +26,11 @@ calcBtn.addEventListener("click", () => {
     }
     const [matrixKofs, matrixVilCh] = formationMatrix();
     const [readyMatrixKofs, readyMatrixVilCh] = GausOdKof(matrixKofs, matrixVilCh);
-    showData(readyMatrixVilCh);
+    const flag = showData(readyMatrixVilCh);
+    if(flag) {
+        entryFinalMatrix(concateData(readyMatrixKofs, readyMatrixVilCh));
+        finalMatrixBtn.disabled = false;
+    }
 })
 
 function formationMatrix() {
@@ -40,12 +50,12 @@ function formationMatrix() {
     for(let i = 0; i < 3; i++) {
         readyMatrixVilCh.push(+matrixVilCh[i].value);
     }
-
     return [readyMatrixKofs, readyMatrixVilCh];
 }
 
 function GausOdKof(kovMatrix, vilChMatrix) {
-    let [kovMatrixCopy, vilChMatrixCopy] = [kovMatrix.slice(), vilChMatrix.slice()];
+    let kovMatrixCopy= [...kovMatrix];
+    let vilChMatrixCopy = [...vilChMatrix];
     let maxRowIndex = 0;
     let maxEl = kovMatrix[0][0];
 
@@ -61,12 +71,10 @@ function GausOdKof(kovMatrix, vilChMatrix) {
     for(let k = 0; k < kovMatrixCopy.length; k++) {
         activeEl = kovMatrixCopy[k][k];
         for(let i = 0; i < kovMatrixCopy.length; i++) {
-            for(let j = 0; j < kovMatrixCopy[i].length; j++) {
-                if(k === i) {
+            if(k === i) {
+                for(let j = 0; j < kovMatrixCopy[i].length; j++) {
                     kovMatrixCopy[i][j] = kovMatrixCopy[i][j] / activeEl;
                 }
-            }
-            if(k === i) {
                 vilChMatrixCopy[i] = vilChMatrixCopy[i] / activeEl;
             }
         }
@@ -74,16 +82,15 @@ function GausOdKof(kovMatrix, vilChMatrix) {
         let savedValue = 0;
         for(let i = 0; i < kovMatrixCopy.length; i++) {
             savedValue = kovMatrixCopy[i][k];
-            for(let j = 0; j < kovMatrixCopy[i].length; j++) {
-                if(k !== i) {
+            if(k !== i) {
+                for(let j = 0; j < kovMatrixCopy[i].length; j++) {
                    kovMatrixCopy[i][j] -= savedValue * kovMatrixCopy[k][j];
                 }
-            }
-            if(k !== i) {
                 vilChMatrixCopy[i] = vilChMatrixCopy[i] - savedValue * vilChMatrixCopy[k];
             }
         }
     }
+    console.log(kovMatrixCopy);
     return [kovMatrixCopy, vilChMatrixCopy];
 }
 
@@ -98,9 +105,15 @@ function showData(resultAr) {
         att.value = "result_p";
         const pEl = document.createElement("p");
         pEl.setAttributeNode(att);
-        pEl.textContent = `X${i + 1} = ${Math.floor(resultAr[i])};`
+        if(isNaN(resultAr[i])) {
+            pEl.textContent = "СЛАР не має розв'язків!";
+            resultCont.appendChild(pEl);
+            return
+        }
+        pEl.textContent = `X${i + 1} = ${resultAr[i].toFixed(3)};`
         resultCont.appendChild(pEl);
     }
+    return true;
 }
 
 clearBtn.addEventListener('click', () => {
@@ -109,6 +122,13 @@ clearBtn.addEventListener('click', () => {
 
 function clearInputs() {
     matrixInputs.forEach(element => {
+        element.value = "";
+    });
+    finalMatrixBtn.disabled = true;
+    entryMatrixBtn.disabled = true;
+    calcBtn.disabled = false;
+    showMatrix("entry-matrix");
+    finalMatrixInputs.forEach(element => {
         element.value = "";
     });
     if(document.querySelectorAll(".result_p")) {
@@ -128,4 +148,52 @@ function closeError() {
     const errorP = document.getElementById("error-modal__text");
     errorMesg.style.right = -window.innerWidth + "px";
     errorP.textContent = "";
-} 
+}
+
+
+entryMatrixBtn.addEventListener("click", () => {
+    showMatrix("entry-matrix");
+    finalMatrixBtn.disabled = false;
+    entryMatrixBtn.disabled = true;
+    calcBtn.disabled = false;
+})
+
+finalMatrixBtn.addEventListener("click", () => {
+    showMatrix("final-matrix");
+    entryMatrixBtn.disabled = false;
+    finalMatrixBtn.disabled = true;
+    calcBtn.disabled = true;
+})
+
+function showMatrix(matrix) {
+    const matrixAr = document.querySelectorAll(".matrix");
+    const curr_matrix = document.getElementById(matrix);
+
+    matrixAr.forEach(matrix_el => {
+        matrix_el.style.display = "none";
+    })
+
+    curr_matrix.style.display = "flex";
+}
+
+function concateData(firArr, secArr) {
+    let resultArray = [...firArr];
+    for(let i = 0; i < firArr.length; i++) {
+        resultArray[i].push(secArr[i]);
+    }
+    const flattenedArray = resultArray.reduce((acc, curr) => acc.concat(curr), []);
+
+    console.log(flattenedArray);
+    return flattenedArray;
+}
+
+function entryFinalMatrix(data) {
+    for(let i = 0; i < finalMatrixInputs.length; i++) {
+        finalMatrixInputs[i].disabled = true;
+        if(data[i].toFixed(3) % 1 !== 0) {
+            finalMatrixInputs[i].value = data[i].toFixed(3);
+        } else {
+            finalMatrixInputs[i].value = data[i];
+        }
+    }
+}
